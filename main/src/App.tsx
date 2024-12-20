88ed4cecd7b50616f30ef551957f0efa003a0d0b
Studbug <nskkid@gmail.com>
2024-12-19 22:16:17 -0800

    Houselceaning, adjust grid implementation

--- main/src/App.tsx
+++ main/src/App.tsx
@@ -1,18 +1,9 @@
 import { useState } from 'react';
-import SheetBloc from './components/spreadsheet'
+import SheetBloc from './components/sheetBloc'
+import GridBloc from './components/gridBloc'
 import './App.css';
 
 function App() {
-  let rowGuides: JSX.Element[] = [];
-  for (let i = 1; i <= 31; i++) {
-    let n = i.toString();
-    rowGuides.push(<div className="Guide rowGuide" style={{gridArea: n+" / 1 / "+n+" / -1"}}></div>);
-  }
-  let colGuides: JSX.Element[] = [];
-  for (let i = 1; i <= 24; i++) {
-    let n = i.toString();
-    colGuides.push(<div className="Guide colGuide" style={{gridArea: "1 / "+n+" / -1 / "+n}}></div>);
-  }
   const spreadsheetData = [
     [{ value: "" }, { value: "" }],
     [{ value: "" }, { value: "" }],
@@ -21,23 +12,21 @@ function App() {
   const addSheetBloc = () => {
     setSheets([...sheets, <SheetBloc cells={spreadsheetData} dark />]);
   }
+
   return (
     <div className="App">
       <header className="App-header">
       </header>
       <main>
-        <section id="Grid">
-          {rowGuides}
-          {colGuides}
-        </section>
-
+        <GridBloc x={24} y={31} />
         {sheets}
+
         <section id="ToolBar">
-        <button onClick={addSheetBloc}>Add Sheet Bloc</button>
-        {/*
-        <button onClick={addTextBloc}>Add Text Bloc</button>
-        <button onClick={addImageBloc}>Add Image Bloc</button>
-        */}
+          <button onClick={addSheetBloc}>Add Sheet Bloc</button>
+          {/*
+          <button onClick={addTextBloc}>Add Text Bloc</button>
+          <button onClick={addImageBloc}>Add Image Bloc</button>
+          */}
         </section>
       </main>
     </div>
